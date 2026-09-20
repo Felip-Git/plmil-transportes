fd0c010c0f74847846608a6a79ae6e2891a28147
Felipe Martins <felipemartins597595@gmail.com>
2026-09-20 01:09:58 -0300

    feat: adiciona cadastro de empresas motoristas e funcionarios

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
-from database import conectar_banco
+from flask import Flask, render_template, request, redirect, url_for
+
+from database import (
+    conectar_banco,
+    buscar_empresas,
+    inserir_empresa,
+    buscar_motoristas,
+    inserir_motorista,
+    buscar_funcionarios,
+    inserir_funcionario
+)
+
 
 app = Flask(__name__)
 
@@ -10,6 +20,75 @@ def home():
     conexao.close()
 
     return "Flask conectado ao PostgreSQL!"
+
+
+@app.route("/empresas")
+def empresas():
+    lista_empresas = buscar_empresas()
+
+    return render_template(
+        "empresas.html",
+        empresas=lista_empresas
+    )
+
+
+@app.route("/empresas/cadastrar", methods=["GET", "POST"])
+def cadastrar_empresa():
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+
+        inserir_empresa(nome)
+
+        return redirect(url_for("empresas"))
+
+    return render_template("cadastrar_empresa.html")
+
+
+@app.route("/motoristas")
+def motoristas():
+    lista_motoristas = buscar_motoristas()
+
+    return render_template(
+        "motoristas.html",
+        motoristas=lista_motoristas
+    )
+
+
+@app.route("/motoristas/cadastrar", methods=["GET", "POST"])
+def cadastrar_motorista():
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+
+        inserir_motorista(nome)
+
+        return redirect(url_for("motoristas"))
+
+    return render_template("cadastrar_motorista.html")
+
+
+@app.route("/funcionarios")
+def funcionarios():
+    lista_funcionarios = buscar_funcionarios()
+
+    return render_template(
+        "funcionarios.html",
+        funcionarios=lista_funcionarios
+    )
+
+
+@app.route("/funcionarios/cadastrar", methods=["GET", "POST"])
+def cadastrar_funcionario():
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+
+        inserir_funcionario(nome)
+
+        return redirect(url_for("funcionarios"))
+
+    return render_template("cadastrar_funcionario.html")
 
 
 if __name__ == "__main__":
